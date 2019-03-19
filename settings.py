cad44397d79a2c0d825c565b2b74c2af9ba97aa8
crabbe <oecfvcavtw48a>
2019-03-19 14:49:39 -0700

    trying to commit settings with stuff for heroku

--- settings.py
+++ settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = ['otree']
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/ for heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
